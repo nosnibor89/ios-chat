6c23269a330b06f07fdb5208b86a5aabf9f611ab
Robinson <nosnibor1989@gmail.com>
2018-07-04 11:27:18 -0400

    Initial handle registration

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 class RegisterViewController: UIViewController {
@@ -33,7 +34,20 @@ class RegisterViewController: UIViewController {
         
         //TODO: Set up a new user on our Firbase database
         
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            
+            if let currentError = error {
+                print(currentError)
+            }else{
+                //Success
+                 print("registration worked")
+                
+                self.performSegue(withIdentifier: AppSegues.goToChat.rawValue, sender: self)
+            }
+            
+           
+        }
 
         
         
